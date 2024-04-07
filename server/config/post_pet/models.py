67d19bf7a3fb4a671c8fed9904c6f7d57da0aee6
Adenidays <133434653+Adenidays@users.noreply.github.com>
2024-04-07 16:18:25 +0300

    fix

--- server/config/post_pet/models.py
+++ server/config/post_pet/models.py
@@ -7,8 +7,9 @@ class PetPost(models.Model):
     title = models.CharField(max_length=255)
     headers = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='uploads/')
+    image = models.ImageField(upload_to='image/')
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
--- server/config/post_pet/views.py
+++ server/config/post_pet/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
+from rest_framework.response import Response
 
 from post_pet.models import PetPost
 from post_pet.serializers import SerializerPost
@@ -8,3 +9,9 @@ class PostViewsSet(viewsets.ModelViewSet):
     queryset = PetPost.objects.all()
     serializer_class = SerializerPost
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def retrieve(self, request, pk=None):
+        queryset = self.get_queryset()
+        post = queryset.filter(id=pk).first()
+        serializer = self.get_serializer(post)
+        return Response(serializer.data)
