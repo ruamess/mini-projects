1f4d7c244eb096b8743d532f1e50ec0688e0176d
Adenidays <133434653+Adenidays@users.noreply.github.com>
2024-04-10 15:41:40 +0300

    fix User

--- server/config/config/settings.py
+++ server/config/config/settings.py
@@ -93,9 +93,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
 ]
+
+CORS_ALLOW_CREDENTIALS = True
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
